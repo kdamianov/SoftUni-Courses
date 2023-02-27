b6f56976a0e78f7340ff1f7328811bf36e18fd53
Kalin Damyanov <106314803+kdamianov@users.noreply.github.com>
2023-02-27 23:09:39 +0200

    Update 07. Bitcoin Mining.js

--- JS Front-End/JS Front-End/01. JS Syntax Fundamentals/Exercise/More Exercise/07. Bitcoin Mining.js
+++ JS Front-End/JS Front-End/01. JS Syntax Fundamentals/Exercise/More Exercise/07. Bitcoin Mining.js
@@ -8,26 +8,27 @@ function solve(inputArr) {
     for (let i = 0; i < inputArr.length; i++) {
         days++;
         minedGrams = inputArr[i];
+        
         if (days % 3 === 0) {
             minedGrams *= 0.7;
         }
 
         totalMoney += minedGrams * 67.51;
 
-
-
         while (totalMoney >= 11949.16) {
             if (dayOfFirstBitcoin === 0) {
                 dayOfFirstBitcoin = days;
             }
-
             gainedBitcoins++;
             totalMoney -= 11949.16;
         }
     }
+    
     console.log(`Bought bitcoins: ${gainedBitcoins}`);
+    
     if (dayOfFirstBitcoin > 0) {
         console.log(`Day of the first purchased bitcoin: ${dayOfFirstBitcoin}`);
     }
+    
     console.log(`Left money: ${totalMoney.toFixed(2)} lv.`)
 }
