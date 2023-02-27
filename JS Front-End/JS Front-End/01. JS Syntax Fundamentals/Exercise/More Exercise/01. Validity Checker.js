bdee52bf6c6f5abdfe9a196ebbca4303939f85f7
Kalin Damyanov <106314803+kdamianov@users.noreply.github.com>
2023-02-27 23:08:00 +0200

    Update 01. Validity Checker.js

--- JS Front-End/JS Front-End/01. JS Syntax Fundamentals/Exercise/More Exercise/01. Validity Checker.js
+++ JS Front-End/JS Front-End/01. JS Syntax Fundamentals/Exercise/More Exercise/01. Validity Checker.js
@@ -19,11 +19,5 @@ function solve(x1, y1, x2, y2) {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
-
-
     }
-
 }
-
-
-solve(2, 1, 1, 1);
