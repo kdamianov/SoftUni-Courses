4ebe7570221d580a3e58f3621f49f854c9e5cf85
Kalin Damyanov <kdamianov@gmail.com>
2023-03-05 15:06:22 +0200

    Update #1

--- JS Front-End/JS Front-End/02.Functions and Statements/Exercise/More exercise/01. Car Wash.js
+++ JS Front-End/JS Front-End/02.Functions and Statements/Exercise/More exercise/01. Car Wash.js
@@ -1,21 +1,25 @@
 function solve(input) {
     let cleanedCar = 0;
+    const add = (a, b) => a + b;
+    const percent = (a, b) => a * (b / 100);
 
     for (const command of input) {
         switch (command) {
             case 'soap':
-                cleanedCar += 10;
+                cleanedCar = add(cleanedCar, 10);
                 break;
             case 'water':
-                cleanedCar += cleanedCar * 0.20;
+                cleanedCar += percent(cleanedCar, 20);
                 break;
             case 'vacuum cleaner':
-                cleanedCar += cleanedCar * 0.25;
+                cleanedCar += percent(cleanedCar, 25);
                 break
             case 'mud':
-                cleanedCar -= cleanedCar * 0.10;
+                cleanedCar -= percent(cleanedCar, 10);
                 break;
         }
     }
     console.log(`The car is ${cleanedCar.toFixed(2)}% clean.`)
 }
+
+solve(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water']);
