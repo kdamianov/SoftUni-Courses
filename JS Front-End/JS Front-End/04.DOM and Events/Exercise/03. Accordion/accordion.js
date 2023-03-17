285569e6414ea0c1f7317d0acde83d064671f253
Kalin Damyanov <kdamianov@gmail.com>
2023-03-17 22:11:13 +0200

    Uploaded files for problem#3 - Exercise

--- JS Front-End/JS Front-End/04.DOM and Events/Exercise/03. Accordion/accordion.js
+++ JS Front-End/JS Front-End/04.DOM and Events/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.getElementsByClassName('button')[0];
+    const content = document.getElementById('extra')
+
+
+    if (button.textContent === 'More') {
+        content.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        content.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
