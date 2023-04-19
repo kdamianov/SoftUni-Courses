2b9d1555a6b931fcbbe57cf3c2d1cd215f4c55b5
Kalin Damyanov <kdamianov@gmail.com>
2023-04-19 23:29:21 +0300

    Updated solve.js

--- JS Front-End/JS Front-End/08.JS Exam 08.04.23/01. Sprint Review/solve.js
+++ JS Front-End/JS Front-End/08.JS Exam 08.04.23/01. Sprint Review/solve.js
@@ -36,10 +36,11 @@ function solve(input) {
             if (!tasksObj.hasOwnProperty(assignee)) {
                 console.log(`Assignee ${assignee} does not exist on the board!`)
             } else {
-                for (const task of tasksObj[assignee]) {
-                    if (task.taskId !== taskId) {
-                        console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
-                    } else {
+                const validId = tasksObj[assignee].filter(e => e.taskId === taskId);
+                if (validId.length === 0) {
+                    console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
+                } else {
+                    for (const task of tasksObj[assignee]) {
                         points[task.status] -= task.estimatedPoints;
                         task.status = newStatus;
                         points[newStatus] += Number(task.estimatedPoints);
@@ -74,3 +75,17 @@ function solve(input) {
         console.log('Sprint was unsuccessful...');
     }
 }
+
+solve([
+    '5',
+    'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
+    'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
+    'Peter:BOP-1211:POC:Code Review:5',
+    'Georgi:BOP-1212:Investigation Task:Done:2',
+    'Mariya:BOP-1213:New Account Page:In Progress:13',
+    'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
+    'Change Status:Mariya:BOP-1290:ToDo',
+    'Remove Task:Mariya:1',
+    'Remove Task:Joro:1',
+]
+);
