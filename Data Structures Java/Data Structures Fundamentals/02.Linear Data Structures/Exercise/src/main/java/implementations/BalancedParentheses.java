67ff2bc5f4b3d16e5f25ad2680f8580f9a7a59da
Kalin Damyanov <kdamianov@gmail.com>
2023-05-01 15:59:00 +0300

    Updated #BalancedParentheses.java

--- Data Structures Java/Data Structures Fundamentals/02.Linear Data Structures/Exercise/src/main/java/implementations/BalancedParentheses.java
+++ Data Structures Java/Data Structures Fundamentals/02.Linear Data Structures/Exercise/src/main/java/implementations/BalancedParentheses.java
@@ -2,6 +2,8 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.ArrayDeque;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
@@ -11,6 +13,39 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        ArrayDeque<Character> parenthesesStack = new ArrayDeque<>();
+
+        for (int i = 0; i < this.parentheses.length(); i++) {
+            char current = parentheses.charAt(i);
+            if (current == '{' || current == '[' || current == '(') {
+                parenthesesStack.push(current);
+            } else {
+                if (parenthesesStack.isEmpty()) {
+                    return false;
+                } else {
+                    char lastParentheses = parenthesesStack.pop();
+                    if (current != getOpposite(lastParentheses)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (!parenthesesStack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    private char getOpposite(char c) {
+        switch (c) {
+            case '(':
+                return ')';
+            case '[':
+                return ']';
+            case '{':
+                return '}';
+            default:
+                return 0;
+        }
     }
 }
