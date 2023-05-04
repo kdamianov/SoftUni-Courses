f4e47d75038ebecd9bd46f1f5bb605936d36e41e
Kalin Damyanov <kdamianov@gmail.com>
2023-05-04 23:20:06 +0300

    Updated #Exercise folder

--- Data Structures Java/Data Structures Fundamentals/03.Trees Representation and Traversal (BFS, DFS)/Exercise/src/main/java/implementations/TheMatrix.java
+++ Data Structures Java/Data Structures Fundamentals/03.Trees Representation and Traversal (BFS, DFS)/Exercise/src/main/java/implementations/TheMatrix.java
@@ -16,11 +16,38 @@ public class TheMatrix {
     }
 
     public void solve() {
-        // TODO: Implement
+        fillMatrix(this.startRow, this.startCol);
+    }
+
+    private void fillMatrix(int row, int col) {
+        if (isOutOfBounds(row, col) || this.matrix[row][col] != this.toBeReplaced) {
+            return;
+            //спира изпълнението на метода (т.нар Дъно)
+        }
+
+        this.matrix[row][col] = fillChar;
+
+        fillMatrix(row + 1, col);
+        fillMatrix(row, col + 1);
+        fillMatrix(row - 1, col);
+        fillMatrix(row, col - 1);
+
+    }
+
+    private boolean isOutOfBounds(int row, int col) {
+
+        return row < 0 || row >= this.matrix.length || col < 0 || col >= this.matrix[row].length;
     }
 
     public String toOutputString() {
-        // TODO: Implement
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int r = 0; r < this.matrix.length; r++) {
+            for (int c = 0; c < this.matrix[r].length; c++) {
+                sb.append(this.matrix[r][c]);
+            }
+            sb.append(System.lineSeparator());
+        }
+
+        return sb.toString().trim();
     }
 }
--- Data Structures Java/Data Structures Fundamentals/03.Trees Representation and Traversal (BFS, DFS)/Exercise/src/main/java/implementations/Tree.java
+++ Data Structures Java/Data Structures Fundamentals/03.Trees Representation and Traversal (BFS, DFS)/Exercise/src/main/java/implementations/Tree.java
@@ -2,48 +2,99 @@ package implementations;
 
 import interfaces.AbstractTree;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Tree<E> implements AbstractTree<E> {
+    private E key; //самият елемент
+    private Tree<E> parent; //насока към Parent Node
+    private List<Tree<E>> children; //пазим всички деца в такава колекция
+
+    public Tree(E key) {
+        this.key = key;
+        this.children = new ArrayList<>();
+    }
 
     @Override
     public void setParent(Tree<E> parent) {
-
+        this.parent = parent;
     }
 
     @Override
     public void addChild(Tree<E> child) {
-
+        this.children.add(child);
     }
 
     @Override
     public Tree<E> getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public String getAsString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        traverseTreeWithRecurrence(sb, 0, this);
+        return sb.toString().trim();
     }
 
     @Override
     public List<E> getLeafKeys() {
-        return null;
+
+        return traverseWithBFS()
+                .stream()
+                .filter(tree -> tree.children.isEmpty()) //няма наследници ==> Leaf
+                .map(Tree::getKey)
+                .collect(Collectors.toList());
+        // връщаме списък с всички Leaf
     }
 
     @Override
     public List<E> getMiddleKeys() {
-        return null;
+        return this.traverseWithBFS()
+                .stream()
+                .filter(tree -> tree.parent != null && !tree.children.isEmpty())
+                .map(Tree::getKey)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Tree<E> getDeepestLeftmostNode() {
-        return null;
+        List<Tree<E>> trees = this.traverseWithBFS();
+
+        int maxPath = 0;
+        Tree<E> deepestLeftmostNode = null;
+
+        for (Tree<E> tree : trees) {
+            if (tree.isLeaf()) {
+                int currentPath = getMaxPathCount(tree);
+                if (currentPath > maxPath) {
+                    maxPath = currentPath;
+                    deepestLeftmostNode = tree;
+                }
+            }
+        }
+        return deepestLeftmostNode;
+    }
+
+    private int getMaxPathCount(Tree<E> tree) {
+        int counter = 0;
+        Tree<E> current = tree;
+        while (current.parent != null) {
+            counter++;
+            current = current.parent;
+        }
+        return counter;
+    }
+
+
+    private boolean isLeaf() {
+        return this.parent != null && this.children.isEmpty();
     }
 
     @Override
@@ -59,6 +110,45 @@ public class Tree<E> implements AbstractTree<E> {
     @Override
     public List<Tree<E>> subTreesWithGivenSum(int sum) {
         return null;
+    }
+
+    private void traverseTreeWithRecurrence(StringBuilder sb, int indent, Tree<E> tree) {
+
+        sb
+                .append(this.getPadding(indent)) // ще даде колко space-а да има (0, 1, 2 и т.н.)
+                .append(tree.getKey()) //стойността на ключа
+                .append(System.lineSeparator());
+
+        for (Tree<E> child : tree.children) {
+            traverseTreeWithRecurrence(sb, indent + 2, child);
+        } //това е и дъното на рекурсията, тк ако няма children, ще спре цикъла
+
+    }
+
+    private String getPadding(int size) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    private List<Tree<E>> traverseWithBFS() {
+        Deque<Tree<E>> queue = new ArrayDeque<>();
+
+        queue.offer(this);
+
+        List<Tree<E>> allNodes = new ArrayList<>();
+
+
+        while (!queue.isEmpty()) {
+            Tree<E> tree = queue.poll();
+            allNodes.add(tree);
+            for (Tree<E> child : tree.children) {
+                queue.offer(child);
+            }
+        }
+        return allNodes;
     }
 }
 
