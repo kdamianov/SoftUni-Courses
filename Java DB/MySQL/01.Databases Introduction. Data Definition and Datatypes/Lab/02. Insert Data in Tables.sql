0f6a8b69979df5449fe56c87ed2e1c1e950b8b44
Kalin Damyanov <kdamianov@gmail.com>
2023-05-10 23:47:10 +0300

    Updated Lab folder

---
+++ Java DB/MySQL/01.Databases Introduction. Data Definition and Datatypes/Lab/02. Insert Data in Tables.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (`id`, `first_name`, `last_name`) VALUES ('1', 'Test1', 'Test1');
+INSERT INTO employees (`id`, `first_name`, `last_name`) VALUES ('2', 'Test2', 'Test2');
+INSERT INTO employees (`id`, `first_name`, `last_name`) VALUES ('3', 'TEst3', 'Test3');
+
