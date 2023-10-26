c5795424bff1aa5216d9634da5f840ff5565d975
Kalin Damyanov <kdamianov@gmail.com>
2023-10-26 18:59:56 +0300

    Initial upload
    Uploaded Spring Security - sample web app

---
+++ Java WEB/Spring Advanced/02.Spring Security/plain_web_app_security/src/main/java/com/example/plain_web_app_security/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.example.plain_web_app_security;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + request.getAttribute("username") + "</h1>");
+        out.println("</body></html>");
+    }
+
+    
+}
